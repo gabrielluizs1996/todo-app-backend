a06e6c6beebbe6309b6bfead439e7b715c925f86
gabrielluizs1996 <gabrielluizs1996@gmail.com>
2021-06-07 11:56:47 -0300

    adicionano backend ao projeto

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const options = {
+    useMongoClient: true,
+    autoIndex: false, // Don't build indexes
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  };
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb://localhost/todo', options)
